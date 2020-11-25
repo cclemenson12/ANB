0c19cdbcd04e9a29bda82ce6e811958f1e94c5e5
Chris Clemenson <15881575+cclemenson12@users.noreply.github.com>
2020-11-25 15:16:14 +0000

    Build Continue

---
+++ EnvPrep/EnvPrep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /opt/contacts_proj/
+mkdir -p /opt/contacts_proj/venv && cd /opt/contacts_proj/venv
+virtualenv -p python3 .
+useradd -p --system --home-dir=/var/opt/contacts_proj --no-create-home --shell=/bin/bash DDjUser
+mkdir -p /var/opt/contacts_proj
+chown DDjUser /var/opt/contacts_proj
+mkdir -p /var/log/contacts_proj
+chown DDjUser /var/log/contacts_proj
+mkdir -p /etc/opt/contacts_proj
+mkdir -p /var/cache/contacts_proj/static
+mkdir -p /var/opt/contacts_proj/media
+chown DDjUser /var/opt/contacts_proj/media
+chown DDjUser /var/cache/contacts_proj/static/
